9ba553c73a51bd4d4e0104ac4b313e0f84536781
kapilchaudhary024 <kapil.chaudhary_iot18@gla.ac.in>
2019-08-24 14:41:46 +0530

    declare all necessary variable

---
+++ src/assignment1/Gravity.java
@@ -0,0 +1,10 @@
+package assignment1;
+
+public class Gravity {
+    public static void main(String[] args) {
+
+        double time = obj.nextDouble();
+        double GRAVITY = 9.8;
+        double speed = GRAVITY*time;
+        double distance = (GRAVITY*time*time)/2;
+}
