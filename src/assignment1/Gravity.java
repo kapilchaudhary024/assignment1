7aad235f0e7dbbc73af1398440a424cda55ca6d6
kapilchaudhary024 <kapil.chaudhary_iot18@gla.ac.in>
2019-08-24 14:42:43 +0530

    import java.util.Scanner

--- src/assignment1/Gravity.java
+++ src/assignment1/Gravity.java
@@ -1,10 +1,12 @@
 package assignment1;
 
+import java.util.Scanner;
 public class Gravity {
     public static void main(String[] args) {
-
+        Scanner obj = new Scanner(System.in);
         double time = obj.nextDouble();
         double GRAVITY = 9.8;
-        double speed = GRAVITY*time;
-        double distance = (GRAVITY*time*time)/2;
+        double speed = GRAVITY * time;
+        double distance = (GRAVITY * time * time) / 2;
+    }
 }
